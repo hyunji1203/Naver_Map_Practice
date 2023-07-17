b1580a28ca7b539e59942e4f8467225cb49a9999
hyunji1203 <hj27720@gmail.com>
2023-07-17 17:24:37 +0900

    feat: 내 위치 불러와 좌표 찍기

--- app/src/main/java/com/example/naver_map_practice/MainActivity.kt
+++ app/src/main/java/com/example/naver_map_practice/MainActivity.kt
@@ -8,6 +8,7 @@ import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -16,14 +17,18 @@ import androidx.core.content.ContextCompat
 import com.example.naver_map_practice.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapView: MapFragment
     private lateinit var naverMap: NaverMap
+    private lateinit var locationSource : FusedLocationSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +40,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             ?: MapFragment.newInstance().also {
                 fragmentManager.beginTransaction().add(R.id.map, it).commit()
             }
+        mapFragment.getMapAsync(this)
+        mapView = mapFragment
+
 
         if (checkLocationService()) {
             permissionCheck()
@@ -98,7 +106,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         permissions: Array<out String>,
         grantResults: IntArray,
     ) {
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+            if(!locationSource.isActivated){
+                Log.d("bbotto", "1")
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            else{
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow
+                Log.d("bbotto", "2")
+            }
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
         if (requestCode == ACCESS_FINE_LOCATION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, LOCATION_PERMISSION_MESSAGE, Toast.LENGTH_LONG).show()
@@ -114,17 +133,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(naverMap: NaverMap) {
+        Log.d("bbotto", "onMapReady 진입")
         this.naverMap = naverMap
 
-        val campos = CameraPosition(
+        val cameraPosition = CameraPosition(
             LatLng(37.515304, 127.103078),
-            3.0,
+            16.0,
         )
-        naverMap.cameraPosition = campos
+        naverMap.cameraPosition = cameraPosition
+        Log.d("bbotto", "센터 위치 설정 ${cameraPosition.target.latitude}, ${cameraPosition.target.longitude}")
+
+        val marker = Marker()
+        marker.position = LatLng(37.515304, 127.103078)
+        Log.d("bbotto", "${marker.position}")
+        marker.map = naverMap
+
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
     }
 
     companion object {
 
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1004
         const val ACCESS_FINE_LOCATION_CODE = 1000
         private const val LOCATION_PERMISSION_MESSAGE = "위치 권한이 승인되었습니다."
         private const val LOCATION_PERMISSION_DENIED_MESSAGE = "위치 권한이 거절되었습니다."
